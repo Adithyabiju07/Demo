67d453ba82b01dcfc434366ac2174accb7dec06a
Adithyabiju07 <adithyabiju011@gmail.com>
2025-11-02 13:24:22 +0530

    Initial Commit

---
+++ value.php
@@ -0,0 +1,19 @@
+<?php
+
+function addFive($num){
+    $num += 25;
+}
+
+$x = 25;
+addFive($x);
+echo "$x  <br>";
+
+function addFiveRef(&$num){
+    $num *= 4;
+}
+
+$x = 25;
+addFiveRef($x);
+echo "$x <br>";
+
+?>
